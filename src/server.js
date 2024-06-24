d1daab4dcf49b515320ad64e4d84e235be599847
ecthon <ecthon@gmail.com>
2024-06-24 15:43:07 -0400

    feature: Estrutura da aplicação - Salvando usuários em memória (Headers)

--- src/server.js
+++ src/server.js
@@ -15,8 +15,36 @@ import http from 'node:http'
 // GET /users => Buscando usuários no back-end
 // POST /users => Criando usuários no back-end
 
+// Aplicação Stateful/Stateless 
+// Stateful => sempre vai ter algum tipo de informação sendo salva em memória
+// Stateless => geralmente salva em dispositivos externos como banco de dados, arquivos de texto e etc...
+
+// JSON - Javascript Object Notation
+
+// Cabeçalhos (Requisição/resposta) => Metadados
+// Informações adicionais
+
+const users = []
+
 const server = http.createServer((req, res) => {
-  const {method, url } = req
+  const { method, url } = req
+
+  if (method === 'GET' && url === '/users') {
+    return res
+    .setHeader('Content-type', 'application/json')
+    .end(JSON.stringify(users))
+  }
+
+  if (method === 'POST' && url === '/users') {
+    users.push({
+      id: 1,
+      name: 'Ecthon Almeida',
+      email: 'ecthon@gmail.com'
+    })
+
+    return res.end('Criação de usuário')
+  }
+
   return res.end('Hello World')
 })
 
