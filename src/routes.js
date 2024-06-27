16e65914d12c60441728bf7dbb1c51a1aafcae18
ecthon <ecthon@gmail.com>
2024-06-27 09:32:01 -0400

    feature: Rotas da aplicação - Criando regex dos parâmetros

--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,22 @@
 import { randomUUID } from 'node:crypto'
 import { Database } from "./database.js"
+import path from 'node:path'
+import { buildRoutePath } from './utils/build-route-path.js'
 
 const database = new Database()
 
 export const routes = [
   {
     method: 'GET',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
       const users = database.select('users')
       return res.end(JSON.stringify(users))
-    }
+    },
   },
   {
     method: 'POST',
-    path: '/users',
+    path: buildRoutePath('/users'),
     handler: (req, res) => {
       const { name, email } = req.body
       const user = {
@@ -24,6 +26,11 @@ export const routes = [
     }
       database.insert('users', user)
       return res.writeHead(201).end()
-    }
+    },
+  },
+  {
+    method: 'DELETE',
+    path: buildRoutePath('/users/:id'),
+    handler: (req, res) = {},
   }
 ]
